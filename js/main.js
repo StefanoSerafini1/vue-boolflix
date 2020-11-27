8afd19c303a24644d5543d380e4747277d7b1aa7
StefanoSerafini <stefanoserafini7@gmail.com>
2020-11-27 13:19:28 +0100

    aggiunta ricerca senza risultati

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var app= new Vue({
   el: '#app',
   data:{
     cerca:'',
+    notfound:'',
     //bandiere per lingua
     bandiere: ['it', 'en', 'es', 'ja', 'de'],
     //creo la lista film dove andrò ad inserire tutti i risultati(dati) dei film
@@ -28,7 +29,7 @@ var app= new Vue({
           .then((film) => {
                  //pusho o assegno nella lista film tutti i risultati(dati) dell'api
                  this.films = film.data.results;
-                 //console.log(this.films);
+                 console.log(this.films);
                });
         axios//richiamo serie
           .get ('https://api.themoviedb.org/3/search/tv',{//chiamata
@@ -45,6 +46,14 @@ var app= new Vue({
                });
                this.cerca = '';
       }
+      //controllo se la ricerca non da alcun risultato
+        if(this.films.length === 0 && this.serieTv.length === 0){
+          this.notfound='no';
+          console.log(this.notfound);
+        }else{
+          this.notfound='si';
+          console.log(this.notfound);
+        }
     },
     //funzione per arrotondare voto da (0 a 10) a (0 a 5)
     stelleVoto(vote) {
